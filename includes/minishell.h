d5c4e6c53e01d8d206c81ae4fe4c96e707087787
thivankas@outlook.com <tsomacha@c3r1p7.hive.fi>
2025-03-26 11:45:18 +0200

    Split and store envp values in KEY=VALUE format

--- includes/minishell.h
+++ includes/minishell.h
@@ -12,17 +12,44 @@
 # include "../ft_libft/ft_printf.h"
 # include "../ft_libft/get_next_line.h"
 
-// typedef struct s_cmd
-// {
-// 	char *text;
-// };
 
+/**
+ *env node will store the environment variable data in the format of key value pair "SHELL=/bin/bash"
+ *envp will split into key,value pairs and will be stored as one key,value pair as a node in a linked
+ *list
+*/
+typedef struct s_env
+{
+	char *name;
+	char *value;
+	struct s_env *next;
+} t_env;
 
-char **ft_split(const char *s, char c);
+/**
+ * main data struture to store shell state
+ * envp - pointer to envp data pass from the main program
+*/
+typedef struct s_shell
+{
+	t_env *env;
+} t_shell;
 
-size_t ft_strlen(const char *s);
+/**
+ *helper fucntions to initiate the program, implementaion on helper.c file
+*/
+int activate_shell(int status, char **envp);
+int init_shell(t_shell *mini, char **envp);
 
-int ft_strncmp(const char *s1, const char *s2, size_t n);
-int ft_strcmp(const char* s1, const char* s2);
+/**
+ *helper functions to create linked list to store the envp values a s key,pair
+ *implementaion on env_ll.c file
+*/
+t_env *create_node(void);
+t_env *add_node(char *env);
+void ft_env_lstadd_back(t_env **lst, t_env *new);
+
+t_env *list_env(char **envp);
+
+int fill_node(t_env *node, char *name, char *value);
 
 #endif
