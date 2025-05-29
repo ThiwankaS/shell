4f00ed8036285ffa2f66c76a8c238cd0fa705b47
Thiwanka Somachandra <thivankas@outlook.com>
2025-05-29 15:26:48 +0300

    Emptying repository to start again

--- srcs/error/syntax_error.c
+++
@@ -1,7 +0,0 @@
-#include "../../includes/shell.h"
-
-int syntax_error(char *msg)
-{
-	ft_putendl_fd(msg, 2);
-	return (1);
-}
--- srcs/utils/cleaners.c
+++
@@ -1,9 +0,0 @@
-#include "../../includes/minishell.h"
-
-void mini_cleaner(t_shell *mini)
-{
-	if(mini->env)
-		clean_env(mini->env);
-	if(mini->cmds)
-		clean_cmds(mini->cmds);
-}
